8579a1fa970c7d01d15907c0dc5acf497eebec89
Julian Degutis de Freitas Garcia <juliang@kyros.com.br>
2018-04-13 14:33:45 -0300

    Thread

--- entrega1/src/br/com/entrega1/server/LogThread.java
+++ entrega1/src/br/com/entrega1/server/LogThread.java
@@ -1,17 +1,16 @@
 package br.com.entrega1.server;
 
-import java.math.BigInteger;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import br.com.entrega1.file.FileWriter;
 
 public class LogThread implements Runnable {
 	
 	private final String LOG_PATH = "resources/log/log.txt";
-	private Map< BigInteger, String > logQueue = new LinkedHashMap< BigInteger, String >();
+	private Queue< String > logQueue = new LinkedList< String >();
 	
-	public LogThread( Map< BigInteger, String > queue ) {
+	public LogThread( Queue< String > queue ) {
 		super();
 		this.logQueue = queue;
 	}
@@ -19,12 +18,14 @@ public class LogThread implements Runnable {
 	@Override
 	public void run() {
 		try {
-			for( Map.Entry< BigInteger, String > entry : logQueue.entrySet() ) {
-				String key = entry.getKey().toString();
-				String value = entry.getValue();
-				String log = key + ";" + value;
-				FileWriter.writeToFile( LOG_PATH, log );
-			} 
+						
+			while( true ) {
+				String log = logQueue.poll();
+				if( log != null ) {
+					FileWriter.writeToFile( LOG_PATH, log );	
+				}
+			}
+			
 		} catch( Exception ex ) {
 			ex.printStackTrace();
 		}
--- entrega1/src/br/com/entrega1/server/ServerUDP.java
+++ entrega1/src/br/com/entrega1/server/ServerUDP.java
@@ -3,6 +3,10 @@ package br.com.entrega1.server;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 import br.com.entrega1.configuration.Configuration;
 import br.com.entrega1.configuration.SocketSetting;
@@ -11,11 +15,11 @@ public class ServerUDP {
 
 	private static DatagramSocket serverSocket;
 	private static SocketSetting mySettings;
+	private static Queue< String > logQueue = new LinkedList< String >();
 
 	public static void main( String args[] ) throws Exception {
 		
 		mySettings = Configuration.serverSettings();
-		
 		serverSocket = new DatagramSocket( mySettings.getPort() );
 		
 		byte[] receiveData = new byte[ 4000 ];
@@ -27,6 +31,7 @@ public class ServerUDP {
 			serverSocket.receive( receivePacket );
 			String sentence = new String( receivePacket.getData() );
 			System.out.println( "RECEIVED: " + sentence );
+			messageToLog( sentence );
 			InetAddress IPAddress = receivePacket.getAddress();
 			int port = receivePacket.getPort();
 			String capitalizedSentence = sentence.toUpperCase();
@@ -35,5 +40,14 @@ public class ServerUDP {
 			serverSocket.send( sendPacket );
 			
 		}
+	}
+	
+	private static void messageToLog( String message ) {
+		List< String > list = Arrays.asList( message.split( " " ) );
+		String log = "";
+		for( String current : list ) {
+			log = log.concat( current );
+		}
+		logQueue.add( log );
 	}
 }
